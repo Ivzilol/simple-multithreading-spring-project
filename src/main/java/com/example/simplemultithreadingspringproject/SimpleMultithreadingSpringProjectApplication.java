def1da3f5701919ad5ecc8a5ce6f24822728bbc3
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-25 16:31:28 +0200

    Initial commit

---
+++ src/main/java/com/example/simplemultithreadingspringproject/SimpleMultithreadingSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplemultithreadingspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMultithreadingSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMultithreadingSpringProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/simplemultithreadingspringproject/entity/User.java
@@ -0,0 +1,26 @@
+package com.example.simplemultithreadingspringproject.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Data
+@Entity
+@Table(name = "user_tbl")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String email;
+    private String gender;
+
+
+}
