5cf9e3f44651dbef00a7854c30338fd8819ace1e
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-03-26 14:56:50 +0200

    Async

--- src/main/java/com/example/simplemultithreadingspringproject/service/UserService.java
+++ src/main/java/com/example/simplemultithreadingspringproject/service/UserService.java
@@ -17,7 +17,6 @@ import java.util.concurrent.CompletableFuture;
 
 @Service
 public class UserService {
-
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
