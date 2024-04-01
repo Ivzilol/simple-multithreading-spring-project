b59b7a4a6cd8b3764d5dd14167fdf06e2cad70dc
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-01 11:09:36 +0300

    Async

--- src/main/java/com/example/simplemultithreadingspringproject/controller/UserController.java
+++ src/main/java/com/example/simplemultithreadingspringproject/controller/UserController.java
@@ -16,6 +16,7 @@ import java.util.concurrent.CompletableFuture;
 
 @RestController
 public class UserController {
+
     private final UserService userService;
     public UserController(UserService userService) {
         this.userService = userService;
