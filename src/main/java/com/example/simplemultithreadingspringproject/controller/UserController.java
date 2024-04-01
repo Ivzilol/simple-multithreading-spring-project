630a972c8c214798f8bac652496fec978028a79c
Ivaylo Alichkov <ivo_ali@abv.bg>
2024-04-01 11:10:12 +0300

    Async

--- src/main/java/com/example/simplemultithreadingspringproject/controller/UserController.java
+++ src/main/java/com/example/simplemultithreadingspringproject/controller/UserController.java
@@ -35,6 +35,7 @@ public class UserController {
     public CompletableFuture<ResponseEntity<?>> findAllUsers() {
         return userService.findAllUsers().thenApply(ResponseEntity::ok);
     }
+
     @GetMapping(value = "/getUsersByThread", produces = "application/json")
     public ResponseEntity<?> getUsers() {
         CompletableFuture<List<User>> users1 = userService.findAllUsers();
